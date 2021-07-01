e04d6a294656d8a360a199e369f9d9085e4ef81c
Bentini13 <bentini113@gmail.com>
2021-07-01 22:52:07 +0300

    Added user update/delete. Added cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 // Init Middleware
 // Body parser Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -15,5 +17,5 @@ app.get("/", (req, res) => {
 // users API routes
 app.use("/api/users", require("./routes/api/users"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -37,5 +37,38 @@ router.post("/", (req, res) => {
 });
 
 // Update user
+router.put("/:id", (req, res) => {
+  const found = usersList.some((user) => user.id === parseInt(req.params.id));
+
+  if (found) {
+    const updUser = req.body;
+    usersList.forEach((user) => {
+      if (user.id === parseInt(req.params.id)) {
+        user.name = updUser.name ? updUser.name : user.name;
+        user.email = updUser.email ? updUser.email : user.email;
+
+        res.json({ msg: "User updated!", user });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: `No user with the id of ${req.params.id}` });
+  }
+});
+
+// Delete user
+router.delete("/:id", (req, res) => {
+  const found = usersList.some((user) => user.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json({
+      msg: "User deleted!",
+      usersList: usersList.filter(
+        (user) => user.id !== parseInt(req.params.id)
+      ),
+    });
+  } else {
+    res.status(400).json({ msg: `No user with the id of ${req.params.id}` });
+  }
+});
 
 module.exports = router;
